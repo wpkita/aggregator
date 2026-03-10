dfe68e42834240c5384eacc7fffd803d1449319a
Will Kita <william.p.kita@gmail.com>
2026-03-10 16:02:14 -0500

    Use proper user agent.

--- src/Aggregator.Aggregators.HackerNews/ServiceCollectionExtensions.cs
+++ src/Aggregator.Aggregators.HackerNews/ServiceCollectionExtensions.cs
@@ -8,7 +8,11 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddHackerNewsAggregator(
         this IServiceCollection services)
     {
-        services.AddHttpClient();
+        services.AddHttpClient(Microsoft.Extensions.Options.Options.DefaultName, client =>
+        {
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0");
+        });
         services.AddScoped<INewsAggregator, HackerNewsAggregator>();
         return services;
     }
